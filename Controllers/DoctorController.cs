da2c9f818d899f00414106bdc05e18ee498e44c1
Olga Te <teteriog@gmail.com>
2024-05-29 21:25:18 +0200

    Adding TempUtility.cs, Temperature.cshtml, DoctorController.cs and changed Program.cs

---
+++ Controllers/DoctorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApp.Models;
+
+namespace MyFirstWebApp.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult Temperature()
+        {
+            return View();
+        }
+
+        public IActionResult Temperature(float temperature, string scale)
+        {
+            string result = TempUtility.Temperature(temperature, scale);
+            ViewData["Message"] = result;
+            return View();
+        }
+    }
+}
---
+++ Models/TempUtility.cs
@@ -0,0 +1,27 @@
+﻿namespace MyFirstWebApp.Models
+{
+    public class TempUtility
+    {
+        public static string Temperature(float temperature, string scale)
+        {
+            if (scale == "Fahrenheit")
+            {
+                // Convert Fahrenheit to Celsius
+                temperature = (temperature - 32) * 5 / 9;
+            }
+
+            if (temperature >= 38.0)
+            {
+                return "You have a fever.";
+            }
+            else if (temperature < 35.0)
+            {
+                return "You have hypothermia.";
+            }
+            else
+            {
+                return "Your temperature is normal.";
+            }
+        }
+    }
+}
